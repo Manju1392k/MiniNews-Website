71fd775cd1bf5ca9f656db5d29c9f829722095e7
Manjunath <sreerammanjunath1392@gmail.com>
2022-06-03 16:39:04 +0530

    Mininews

--- MiniNewsApp/models.py
+++ MiniNewsApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blogs(models.Model):
+    Blog_Tittle = models.CharField(max_length=50)
+    Blog_Time = models.DateTimeField(auto_now=True)
+    Blog_Description = models.CharField(max_length=350)
+    Blog_AuthorName = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.Blog_Tittle
